175c882ad99ce84387b7fee9327699286349440d
matbachowski <72310751+matbachowski@users.noreply.github.com>
2023-04-25 16:20:01 +0200

    1

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+    @EventListener (ApplicationReadyEvent.class)
+    public void get() {
+        System.out.println("Hello");
+    }
+
+}
